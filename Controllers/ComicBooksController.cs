cc79f0986a944334417d88f2c879ac8cf2b2dcc7
Adel Ati <adel.el.ati@emt.inrs.ca>
2016-07-21 00:49:04 -0400

    Updating Comic Books View

--- Controllers/ComicBooksController.cs
+++ Controllers/ComicBooksController.cs
@@ -10,7 +10,19 @@ namespace ComicBookGallery.Controllers
     {
 		public ActionResult detail(){
 
+			ViewBag.SeriesTitle = "The Amazing Spider Man";
+			ViewBag.IssueNumber = 700;
+			ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+			ViewBag.ArtistsList = new string[] {
+				"Script: Dan Slott",
+				"Pencils: Humberto Ramos",
+				"Inks: Victor Olazaba",
+				"Colors: Edgar Delgado",
+				"Letters: Chris Eliopoulos"
+			};
+
 			return this.View ();
+
 
 		}
     }
